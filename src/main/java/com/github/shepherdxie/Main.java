6bddf652cf4facec3af85d2d0dc7f5ae78edbbc0
Shepherd Xie <shepherd.xie@gmail.com>
2024-01-25 08:56:54 +0800

    update

---
+++ src/main/java/com/github/shepherdxie/Main.java
@@ -0,0 +1,11 @@
+package com.github.shepherdxie;
+
+/**
+ * @author Shepherd Xie
+ * @since 2024/1/24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
